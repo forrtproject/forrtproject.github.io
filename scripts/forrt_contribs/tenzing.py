522ce1f8506949e51153c1cbdf7240c07495a68f
emily-j-f <emilyjfriedel@gmail.com>
2025-04-30 15:52:34 +1000

    Update tenzing.py - Putting Flavio last on the contributors page (#342)
    At his request, actually at his several requests and I'm sick of hearing about it so I've done it now and he owes me big time.

--- scripts/forrt_contribs/tenzing.py
+++ scripts/forrt_contribs/tenzing.py
@@ -83,7 +83,10 @@ merged_data['special_role'] = False
 merged_data = pd.concat([df_roles, merged_data], axis=0)
 merged_data.reset_index(drop=True, inplace=True)
 
-merged_data = merged_data.sort_values(by='Surname')
+# Sort by surname, placing 'Azevedo' last because he is a massive pain in the backside
+merged_data['sort_order'] = merged_data['Surname'].apply(lambda x: 'ZZZZZZZZ' if str(x).strip() == 'Azevedo' else str(x).strip())
+merged_data = merged_data.sort_values(by='sort_order')
+merged_data = merged_data.drop(columns='sort_order')
 
 # Strip spaces from 'ORCID iD' in merged data
 merged_data['ORCID iD'] = merged_data['ORCID iD'].str.strip()
